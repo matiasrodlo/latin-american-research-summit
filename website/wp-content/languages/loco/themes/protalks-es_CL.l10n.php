1f6dcf460f94cbc42f57b57dcf6c8df040988436
Matías Rodlo <52969662+matiasrodlo@users.noreply.github.com>
2025-08-30 20:32:04 -0400

    Add WordPress plugins and language files

---
+++ website/wp-content/languages/loco/themes/protalks-es_CL.l10n.php
@@ -0,0 +1,3 @@
+<?php
+return ['project-id-version'=>'ProTalks 1.0','report-msgid-bugs-to'=>'https://wordpress.org/support/theme/protalks','last-translator'=>'','language-team'=>'Español de Chile','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','pot-creation-date'=>'2025-05-20T12:31:38+02:00','po-revision-date'=>'2025-08-19 18:11+0000','x-generator'=>'Loco https://localise.biz/','x-domain'=>'protalks
+','language'=>'es_CL','plural-forms'=>'nplurals=2; plural=n != 1;','x-loco-version'=>'2.8.0; wp-6.8.2; php-8.1.33','messages'=>[]];
